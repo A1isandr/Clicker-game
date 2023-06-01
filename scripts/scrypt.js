8f662cfc986d8fa29e4457a212acf136a9dba5ad
Alex Pi <alexandrpi345@gmail.com>
2023-06-01 20:02:14 +0300

    Первый коммит

---
+++ scripts/scrypt.js
@@ -0,0 +1,21 @@
+let balance = document.getElementById('balance');
+let save_button = document.getElementById('save');
+let click_area = document.getElementsByClassName('click-area');
+
+if (!localStorage.getItem('money')) {
+  let money = 0;
+  balance.textContent = money + ' $';
+} else {
+  let money = parseInt(localStorage.getItem('money'));
+  balance.textContent = money + ' $';
+}
+
+click_area[0].onclick = function () {
+  money += 1;
+  balance.textContent = money + ' $';
+  localStorage.setItem('money', money);
+};
+
+save_button.onclick = function () {
+  localStorage.setItem('money', money);
+};
