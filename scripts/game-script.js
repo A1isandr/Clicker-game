5f84f5fa8859261179f454af39178da3dfbbe443
Alex Pi <alexandrpi345@gmail.com>
2023-06-11 00:29:56 +0300

    Начал работать над game-script Добавил учет количество улучшений (начальная версия)

--- scripts/game-script.js
+++ scripts/game-script.js
@@ -1,6 +1,9 @@
 var balance = document.getElementById('balance');
 var click_area = document.getElementsByClassName('click-area');
+var upgrade_purchase_button = document.getElementById('purchase-button');
+var upgrade_amount_span = document.getElementById('amount');
 
+// Восстановление прогресса
 if (!localStorage.getItem('money')) {
   var money = 0;
   balance.textContent = money + ' $';
@@ -9,8 +12,23 @@ if (!localStorage.getItem('money')) {
   balance.textContent = money + ' $';
 }
 
+if (!localStorage.getItem('upgrade_amount')) {
+  var upgrade_amount = 0;
+  upgrade_amount_span.textContent = 'Количество: ' + upgrade_amount;
+} else {
+  var upgrade_amount = parseInt(localStorage.getItem('upgrade_amount'));
+  upgrade_amount_span.textContent = 'Количество: ' + upgrade_amount;
+}
+
+// onclick-функции
 click_area[0].onclick = function () {
-  money += 1;
+  money++;
   balance.textContent = money + ' $';
   localStorage.setItem('money', money);
 };
+
+upgrade_purchase_button.onclick = function () {
+  upgrade_amount++;
+  upgrade_amount_span.textContent = 'Количество: ' + upgrade_amount;
+  localStorage.setItem('upgrade_amount', upgrade_amount);
+}
