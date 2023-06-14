1a43d505d6f920c84b5286305c553b163be1d554
Alex Pi <alexandrpi345@gmail.com>
2023-06-15 01:03:06 +0300

    Добавил переключение вкладок меню, немного переработал html

--- scripts/nav-menu.js
+++ scripts/nav-menu.js
@@ -1,8 +1,22 @@
 const navItems = document.querySelectorAll('.nav-scroller__item');
+const contentBlocks = document.querySelectorAll('.content');
 
 navItems.forEach(item => {
   item.addEventListener('click', () => {
+    
+    // Делаем активным
     navItems.forEach(item => item.classList.remove('active'));
     item.classList.add('active');
+
+    const contentId = item.dataset.content;
+    
+    // Скрываем все блоки контента
+    contentBlocks.forEach(block => {
+      block.style.display = 'none';
+    });
+    
+    // Отображаем выбранный блок контента
+    const selectedContent = document.getElementById(contentId);
+    selectedContent.style.display = 'block';
   });
 });
